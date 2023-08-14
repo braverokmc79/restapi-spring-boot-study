89de46aad988c49b44ed4e2ff059545297bcaccc
junhochoi <braverokmc79@gmail.com>
2023-08-14 18:28:16 +0900

    다국어 처리를 위한 internationalization 구현 방법

--- src/main/java/com/example/restfullwebservice/RestfullWebServiceApplication.java
+++ src/main/java/com/example/restfullwebservice/RestfullWebServiceApplication.java
@@ -1,7 +1,13 @@
 package com.example.restfullwebservice;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfullWebServiceApplication {
@@ -9,5 +15,16 @@ public class RestfullWebServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfullWebServiceApplication.class, args);
 	}
+
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver localeResolver =new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.KOREA);
+		return localeResolver;
+	}
+
+
+
 
 }
--- src/main/java/com/example/restfullwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfullwebservice/helloworld/HelloWorldController.java
@@ -1,9 +1,18 @@
 package com.example.restfullwebservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+
+    @Autowired
+    private MessageSource messageSource;
+
 
     //GET
     // /hello-world (endpoint)
@@ -20,6 +29,14 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World  , %s"  , name) );
     }
 
+
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language" , required = false)   Locale locale){
+
+        return  messageSource.getMessage("greeting.message", null, locale);
+
+    }
 
 
 
