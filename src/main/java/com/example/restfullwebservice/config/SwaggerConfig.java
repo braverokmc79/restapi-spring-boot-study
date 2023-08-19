88860e6757ab62bc1c0ebf7201a2106e601513ee
junhochoi <braverokmc79@gmail.com>
2023-08-19 22:47:32 +0900

    ãsswagger 라이블브러리 추가

--- src/main/java/com/example/restfullwebservice/config/SwaggerConfig.java
+++ src/main/java/com/example/restfullwebservice/config/SwaggerConfig.java
@@ -1,13 +1,5 @@
 package com.example.restfullwebservice.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import lombok.RequiredArgsConstructor;
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-
 //**
 //https://velog.io/@kjgi73k/Springboot3에-Swagger3적용하기
 
@@ -18,25 +10,16 @@ import org.springframework.context.annotation.Configuration;
  * https://dev-youngjun.tistory.com/258
  *
  */
-@OpenAPIDefinition(
-        info=@Info(title="API 명세서",
-                description = "Spring boot API",
-                version = "v1"
-        )
-)
-@Configuration
-@RequiredArgsConstructor
+//@OpenAPIDefinition(
+//        info=@Info(title="API 명세서",
+//                description = "Spring boot API",
+//                version = "v1"
+//        )
+//)
+//@Configuration
+//@RequiredArgsConstructor
 public class SwaggerConfig {
 
-        @Bean
-        public GroupedOpenApi chatOpenApi() {
-                String[] paths = {"/v1/**"};
-
-                return GroupedOpenApi.builder()
-                        .group("채팅서비스 API v1")
-                        .pathsToMatch(paths)
-                        .build();
-        }
 
 
 
