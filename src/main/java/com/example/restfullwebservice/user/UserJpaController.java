7e09221ddb05e3cbbe2ae34b519a70e023daee80
braverokmc79 <braverokmc79@gmail.com>
2023-11-14 23:35:41 +0900

    ãJPA 를 이용한 사용자 목록 조회  get HttHTTP  Method

--- src/main/java/com/example/restfullwebservice/user/UserJpaController.java
+++ src/main/java/com/example/restfullwebservice/user/UserJpaController.java
@@ -1,12 +1,19 @@
 package com.example.restfullwebservice.user;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/jpa")
@@ -22,6 +29,50 @@ public class UserJpaController {
         return userRepository.findAll();
 
     }
+
+
+    /**
+     * [Spring] RestAPI와 Hateoas로 링크 삽입하기*
+     *
+     * https://katastrophe.tistory.com/160
+     * @param id
+     * @return
+     *
+     *
+     * http://localhost:8088/jpa/users/1
+     * 
+     * 출력 예
+     *
+     * {
+     *     "name": "User1",
+     *     "joinDate": "2023-11-13T15:00:00.000+00:00",
+     *     "ssn": "701010-11111",
+     *     "_links": {
+     *         "all-users": {
+     *             "href": "http://localhost:8088/jpa/users"
+     *         }
+     *     }
+     * }
+     */
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable  int id){
+        Optional<User> user =userRepository.findById(id);
+
+        if(user.isEmpty()){
+            throw new UserNotFoundException(String.format("ID{%s} not found", id));
+        }
+
+       // Resource
+        EntityModel<User> userEntityModel=EntityModel.of(user.get());
+        WebMvcLinkBuilder linkTo= linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        userEntityModel.add(linkTo.withRel("all-users"));
+
+        return userEntityModel;
+    }
+
+
+
 
 
 
